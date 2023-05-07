5fcbfdccaa44671ef27a51914e9fcd5a66d9cd7b
Dolphin2016 <evgenij55555@gmail.com>
2023-05-07 20:46:18 +0500

    Dynamically Loading Assemblies and Executing Methods

---
+++ DynamicLoadAndExecute.Console/DemoAssemblyLoadContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Loader;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class DemoAssemblyLoadContext : AssemblyLoadContext
+{
+    private AssemblyDependencyResolver _resolver;
+
+    public DemoAssemblyLoadContext(string mainAssemblyToLoadPath) : base(isCollectible: true)
+    {
+        _resolver = new AssemblyDependencyResolver(mainAssemblyToLoadPath);
+    }
+}
---
+++ DynamicLoadAndExecute.Console/Program.Helpers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+partial class Program
+{
+    static void OutputAssemblyInfo(Assembly a)
+    {
+        WriteLine("FullName: {0}", a.FullName);
+        WriteLine("Location: {0}", Path.GetDirectoryName(a.Location));
+        WriteLine("IsCollectible: {0}", a.IsCollectible);
+        WriteLine("Defined types:");
+        foreach (TypeInfo info in a.DefinedTypes)
+        {
+            if (!info.Name.EndsWith("Attributes"))
+            {
+                WriteLine(" Name: {0}, Members: {1}",
+                    info.Name, info.GetMembers().Count());
+            }
+        }
+        WriteLine();
+    }
+}
