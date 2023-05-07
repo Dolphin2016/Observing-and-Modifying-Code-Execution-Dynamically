f024a80907ad7218c3f27b28c2a26f3018c041dd
Dolphin2016 <evgenij55555@gmail.com>
2023-05-07 19:06:22 +0500

    Reading Assembly Metadata

---
+++ WorkingWithReflection/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+
+WriteLine("Assembly metadata:");
+Assembly? assembly = Assembly.GetEntryAssembly();
+
+if (assembly is null)
+{
+    WriteLine("Failed to get entry assembly.");
+    return;
+}
+
+WriteLine($" Full name: {assembly.FullName}");
+WriteLine($" Location: {assembly.Location}");
+WriteLine($" Entry point: {assembly.EntryPoint?.Name}");
+
+IEnumerable<Attribute> attributes = assembly.GetCustomAttributes();
+WriteLine($" Assembly-level attributes:");
+foreach (Attribute a in attributes)
+{
+    WriteLine($" {a.GetType()}");
+}
+
+AssemblyInformationalVersionAttribute? version = assembly
+    .GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+
+WriteLine($" Version: {version?.InformationalVersion}" );
+
+AssemblyCompanyAttribute? company = assembly
+    .GetCustomAttribute<AssemblyCompanyAttribute>();
+
+WriteLine($" Company: {company?.Company}");
